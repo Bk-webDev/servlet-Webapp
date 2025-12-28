b646061725173f9b241d495bf7408e449c3c3141
Bk-webDev <birukbl178@gmail.com>
2025-12-28 20:41:43 +0300

    Create Student.java

---
+++ src/main/java/ model/Student.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Student {
+    public String name;
+    public String email;
+    public int year;
+
+    public Student(String n, String e, int y) {
+        name = n;
+        email = e;
+        year = y;
+    }
+}
